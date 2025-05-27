16e14be0ed400885fe57ecd66e47fecc6b999f7e
Leo Medina <leo@suyai.local>
2025-05-27 14:58:42 +1000

    calculate SNR in case it is not there

--- src/main_new_data.jl
+++ src/main_new_data.jl
@@ -6,7 +6,7 @@ using HDF5
 include("preprocessing.jl")
 include("processing.jl")
 
-data_folder = "~/../neuroeng/data/UV_uERG"
+data_folder = "../../../neuroeng/data/UV_uERG"
 # parameters
 filter = Dict(
 	"type" => "BPF",
@@ -55,9 +55,9 @@ datasets = [
 	"MR-0658",
 	"MR-0687-t1",
 	# WT 9m female
-	#"MR-0683", #NEED TO GET SRN FILE!
+	"MR-0683", 
 	# WT 9m male
-	#"MR-0670", #NEED TO GET SNR FILE!
+	"MR-0670", 
 	"MR-0682",
 	# 5xFAD 3m female
 	"MR-0644",
@@ -67,6 +67,7 @@ datasets = [
 	# 5xFAD 3m male
 	"MR-0661",
 	"MR-0663",
+	"MR-0666",
 	"MR-0667",
 	# 5xFAD 6m female
 	"MR-0659-t1",
@@ -76,12 +77,25 @@ datasets = [
 	"MR-0690-t2",
 	# 5xFAD 6m male
 	"MR-0656",
+	"MR-0657-t1",
+	"MR-0657-t2",
 	"MR-0674",
 	# 5xFAD 9m female
 	"MR-0646",
 	"MR-0647",
+	"MR-0684",
+	"MR-0693-t2",
+	"MR-0694-t2",
+	"MR-0695-t2",
 	# 5xFAD 9m male
-	"MR-0681"
+	"MR-0681",
+	# TgXBP1s 9m female
+	"MR-0709-t2",
+	"MR-0710-t1",
+	"MR-0712-t2",
+	# TgXBP1s 9m male
+	"MR-0708-t1",
+	"MR-0711-t2"
 ]
 
 for electrode_filter in e_f_list
@@ -96,7 +110,7 @@ for electrode_filter in e_f_list
 			#check if snr file exists
 			if !isfile(data_folder*"/"*dataset*"_SNR.h5")
 				println("SNR file not found for dataset: ", dataset)
-				continue
+				compute_and_save_snr(dataset, resampling_rate)
 			end
 			snr_file = h5open(data_folder*"/"*dataset*"_SNR.h5", "r")
 			threshold = parse(Float64, electrode_filter[5:end])
